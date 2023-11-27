34be725764448ed9efa475f6a3b0dcf11947f6cd
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-11-26 23:03:53 -0600

    Pushing to Heroku

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pwa-text-editor
+# PWA Text Editor
+ This is a text editor that runs in the browser. The is a single-page application that meets the PWA criteria.
+
+ ## Links 
+ - [Repo Links](https://github.com/lopezivan763/pwa-text-editor)
+
+ - [Live Link]()
--- client/src-sw.js
+++ client/src-sw.js
@@ -28,38 +28,15 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  // Match specific criteria for assets to be cached (e.g., images, CSS, JS)
-  ({ request }) =>
-    request.destination === 'image' ||
-    request.destination === 'style' ||
-    request.destination === 'script',
-  new CacheFirst({
-    cacheName: 'assets-cache', // Change this to your desired cache name for assets
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
     plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 50, // Maximum number of entries to cache
-        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for 7 days
       }),
     ],
   })
 );
-
-offlineFallback({
-  strategy: pageCache,
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
-    }),
-  ],
-  request: ({ event }) => {
-    // Provide a default response when offline
-    return caches.match('/offline.html');
-    // Replace '/offline.html' with the appropriate path to your offline page
-  },
-});
